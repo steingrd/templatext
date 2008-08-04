50e8fb9fd5add758a76b1f85d91605c7e94ef359
steingrim.dovland@gmail.com <steingrim.dovland@gmail.com@576fb5a9-9b51-0410-92df-f1a2cc439826>
2008-08-04 18:19:59 +0000

    removed method in Variable that is implemented in java.util.Arrays
    git-svn-id: https://templatext.googlecode.com/svn/trunk/templatext@33 576fb5a9-9b51-0410-92df-f1a2cc439826

--- templatext-core/src/main/java/org/templatext/template/Variable.java
+++ templatext-core/src/main/java/org/templatext/template/Variable.java
@@ -1,7 +1,6 @@
 package org.templatext.template;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import org.templatext.template.core.VariableChain;
 
@@ -23,15 +22,7 @@ public class Variable {
 		
 		String[] parts = variable.split("\\.");
 		name = parts[0];
-		chain = new VariableChain(collect(parts));
-	}
-
-	private List<String> collect(String[] parts) {
-		List<String> chain = new ArrayList<String>();
-		for (int i = 1; i < parts.length; i++) {
-			chain.add(parts[i]);
-		}
-		return chain;
+		chain = new VariableChain(Arrays.asList(parts));
 	}
 
 	public String getName() {
